e6e6470cae951e943eb83a958b9f373cccaaea40
Dano1307 <dani.montanari13@gmail.com>
2023-08-08 18:42:32 +0200

    Terzo commit fatto da Intellij

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -19,6 +19,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Bro cmon!");
         stage.setScene(scene);
         stage.show();
+
+        System.out.println("Doing other stuffs...");
     }
 
     public static void main(String[] args) {
