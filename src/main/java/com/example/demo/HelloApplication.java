709430af519eecd64150333b448d851df2885f27
Dano1307 <dani.montanari13@gmail.com>
2023-08-08 18:25:09 +0200

    Secondo commit

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -10,6 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        for(int i=0; i<10; i++){
+            System.out.println("Doing stuffs...");
+        }
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
